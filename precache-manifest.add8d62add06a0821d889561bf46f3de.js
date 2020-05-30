93e95a5a4acb9fdcd4a64c702c5ff436dad03f89
shrushti2000 <shrushtipolekar@gmail.com>
2020-05-30 15:13:17 +0530

    Updates

---
+++ precache-manifest.add8d62add06a0821d889561bf46f3de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ba1e34dabda56ec80c5765d1d9f54a",
+    "url": "/Food-Recipe-website/index.html"
+  },
+  {
+    "revision": "69315444a189c80cea57",
+    "url": "/Food-Recipe-website/static/css/main.98ae07c2.chunk.css"
+  },
+  {
+    "revision": "9fc18d82e389cd0d3836",
+    "url": "/Food-Recipe-website/static/js/2.2ff27a14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Food-Recipe-website/static/js/2.2ff27a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69315444a189c80cea57",
+    "url": "/Food-Recipe-website/static/js/main.e776cd94.chunk.js"
+  },
+  {
+    "revision": "84d5ef0006055624ea6f",
+    "url": "/Food-Recipe-website/static/js/runtime-main.9f5e31e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Food-Recipe-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
